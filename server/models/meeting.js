ccbba521109998717faf95b7f66459bc7635b74b
Gokberk Otlu <gokberkotlu@gmail.com>
2021-12-22 01:30:37 +0300

    feat: add meeting model

--- server/models/meeting.js
+++ server/models/meeting.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 
-const meetingSchema = mongoose.Schema();
+// subject
+// date
+// start_time
+// end_time
+// participants
+const meetingSchema = mongoose.Schema({
+    subject: {
+        type: String,
+        required: [true, 'Enter meeting subject']
+    },
+    data: {
+        type: Date,
+        required: [true, 'Enter meeting date']
+    },
+    start_time: {
+        type: String,
+        required: [true, 'Enter start time']
+    },
+    end_time: {
+        type: String,
+        required: [true, 'Enter end time']
+    },
+    participants: {
+        type: Array,
+        required: [true, 'Enter the participants'],
+        validate(value) {
+            if(!(value.length > 0)) {
+                throw new Error('At least one participant must be entered');
+            }
+        }
+    }
+});
 
 const Meeting = mongoose.model('meeting', meetingSchema);
 
